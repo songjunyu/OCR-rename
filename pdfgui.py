e18d46a6a2d856cd8f63286bc650bf7d7af1a922
sjy <1434508535@qq.com>
2020-12-06 21:34:14 +0800

    GUI界面程序 V7.0 重大更新，解决gui界面卡顿

--- pdfgui.py
+++ pdfgui.py
@@ -4,12 +4,12 @@ from pdf import BatchSM
 from rcordtoexcl import Rcord
 from pdfaddimg import pdf2pic
 from pdfrename import BatchRename
-
+import threading
 def selectPath(p_):
     path_ = askdirectory()
     p_.set(path_)
-
-def sm():
+def Processsm():
+    print("子进程")
     print("开始合并")
     demo = BatchSM(path1.get(), path2.get())  # 构建合并文件类，参数为源文件夹和目标文件夹
     demo.rem()
@@ -19,42 +19,104 @@ def sm():
     L1['bg'] = "green"
     L3['text'] = "点击按钮进行著录->"
     B1['text'] = "附件pdf著录"
-    # t = threading.Thread(target=demo.rem)
-    # # 守护线程
-    # t.setDaemon(True)
-    # t.start()
-
-
-def record():
+    B1['state'] = 'normal'
+def Processrecord():
+    print("子进程")
     global jnpath
-    print(path2.get(), "开始著录") # path2为合并后附件文件夹
-    demo = Rcord(ry.get(),path1.get())          # 著录实现
-    jnpath=demo.rcord(path2.get())
+    print(path2.get(), "开始著录")  # path2为合并后附件文件夹
+    demo = Rcord(ry.get(), path1.get())  # 著录实现
+    jnpath = demo.rcord(path2.get())
     print(path2.get(), "完成著录")
-    L1['text']="毕业照片"
-    L1['bg']="yellow"
-    L3['text']="选择照片路径高亮->"
+    L1['text'] = "毕业照片"
+    L1['bg'] = "yellow"
+    L3['text'] = "选择照片路径高亮->"
     B1['command'] = addimg
     B1['text'] = "添加照片"
-
-def addimg():
+    B1['state'] = 'normal'
+def Processaddimg():
+    print("子进程")
     print("添加照片")
-    pdf2pic(path2.get(),path1.get(),jnpath)
+    pdf2pic(path2.get(), path1.get(), jnpath)
     print("添加照片完成")
     L3['text'] = "点击按钮进行改名->"
     B1['command'] = rename
+    L1['bg'] = 'SystemButtonFace'
     B1['text'] = "改名"
+    B1['state'] = 'normal'
+def Processrename():
+    print("子进程")
+    print("改名")
+    demo = BatchRename()  # 重命名类
+    demo.rename(path2.get())
+    print("改名完成")
+    B1['command'] = root.quit
+    B1['text'] = "退出程序"
+    B1['state'] = 'normal'
+def sm():
+    # print("开始合并")
+    # demo = BatchSM(path1.get(), path2.get())  # 构建合并文件类，参数为源文件夹和目标文件夹
+    # demo.rem()
+    # print("完成合并")
+    # B1['command'] = record
+    # L1['text'] = "案卷保存"
+    # L1['bg'] = "green"
+    # L3['text'] = "点击按钮进行著录->"
+    # B1['text'] = "附件pdf著录"
+    B1['text'] = "pdf合并中"
+    B1['state']='disabled'
+    t = threading.Thread(target=Processsm)
+    # # 守护线程
+    t.setDaemon(True)
+    t.start()
+
+
+def record():
+    # global jnpath
+    # print(path2.get(), "开始著录") # path2为合并后附件文件夹
+    # demo = Rcord(ry.get(),path1.get())          # 著录实现
+    # jnpath=demo.rcord(path2.get())
+    # print(path2.get(), "完成著录")
+    # L1['text']="毕业照片"
+    # L1['bg']="yellow"
+    # L3['text']="选择照片路径高亮->"
+    # B1['command'] = addimg
+    # B1['text'] = "添加照片"
+    B1['text'] = "著录中"
+    B1['state'] = 'disabled'
+    t = threading.Thread(target=Processrecord)
+    # # 守护线程
+    t.setDaemon(True)
+    t.start()
+def addimg():
+    # print("添加照片")
+    # pdf2pic(path2.get(),path1.get(),jnpath)
+    # print("添加照片完成")
+    # L3['text'] = "点击按钮进行改名->"
+    # B1['command'] = rename
+    # B1['text'] = "改名"
+    B1['text'] = "添加照片中"
+    B1['state'] = 'disabled'
+    t = threading.Thread(target=Processaddimg)
+    # # 守护线程
+    t.setDaemon(True)
+    t.start()
 def rename():
     # print('目标路径：%s' % p_.get())
     # demo = BatchSM(path1.get(), path2.get())  # 构建合并文件类，参数为源文件夹和目标文件夹
     # print(path1.get(), "pdf附件路径")
 
-    print("改名")
-    demo = BatchRename() # 重命名类
-    demo.rename(path2.get())
-    print("改名完成")
-    B1['command'] = root.quit
-    B1['text'] = "退出程序"
+    # print("改名")
+    # demo = BatchRename() # 重命名类
+    # demo.rename(path2.get())
+    # print("改名完成")
+    # B1['command'] = root.quit
+    # B1['text'] = "退出程序"
+    B1['text'] = "改名中"
+    B1['state'] = 'disabled'
+    t = threading.Thread(target=Processrename)
+    # # 守护线程
+    t.setDaemon(True)
+    t.start()
 
 #     e.delete(0, END)  # 获取完信息，清楚掉输入框的
 
